8db24f62a4842c7ad167dae85997443ab5847204
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-23 10:14:50 +0530

    Add files via upload

---
+++ Nnums.java
@@ -0,0 +1,26 @@
+package Guvi;
+
+public class Nnums {
+	public static void main(String[] args) {
+		
+        int input = 6;
+        int sum = 0;
+        
+        for(int i = 1; i <= input; i++)
+        {
+            sum = sum + i;    // LINE A
+            System.out.println("Sum after adding " + i + " is : " + sum); 
+        }
+        
+        System.out.println();
+        System.out.println("Sum of numbers till " + input + " is " + sum); // LINE B
+    
+    }
+
+
+}
+
+
+
+
+
